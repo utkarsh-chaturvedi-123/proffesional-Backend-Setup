0f39420c53cf0a3205d415e6ea0196b14beb1c08
utkarsh chaturvedi <utkarshchaturvedi890@gmail.com>
2024-03-28 13:07:36 +0530

    Connect to Mongoose atlas

--- src/db/index.js
+++ src/db/index.js
@@ -6,6 +6,7 @@ const connectDB = async ()=>{
     try {
         const connectionInstance =  await mongoose.connect(`${process.env.MONGODB_URI}/${DATABASE_NAME}`);
         console.log(`Mongodb connected  !! DB HOST: ${connectionInstance.connection.host}`)
+        /*connectionInstance.connection.host -> this will show the host server because server are different (production,development,testing etc)*/
     } catch (error) {
         console.log(`Mongodb connection Failed ! ${error}`)
         process.exit(1);
