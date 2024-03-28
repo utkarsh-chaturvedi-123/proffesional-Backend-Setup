11e67f41951d75b98973208dc76edffce2abf8d3
utkarsh chaturvedi <utkarshchaturvedi890@gmail.com>
2024-03-28 09:47:17 +0530

    Connect to Mongoose atlas

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // require('dotenv').config({path: './env'})
-import dotenv from "dotenv"
 
+/*Above syntax will also work but we are using proffesional approach by using import for following the proffessional syntax*/
+import dotenv from "dotenv"
 import connectDB from "./db/index.js";
 
 dotenv.config({
