5d6b58dec40cc78dc3f149e6e7d825af072433e3
utkarsh chaturvedi <utkarshchaturvedi890@gmail.com>
2024-03-31 23:05:24 +0530

    after add some notes as a comment

--- src/app.js
+++ src/app.js
@@ -1,5 +1,19 @@
-import { Express } from "express";
+/* CORS middleware or CORS ---->>CORS allows you to configure the web API's security. It has to do with allowing other domains to make requests against your web API. you need it for authorized resource sharing with external third parties.  Browsers enforce CORS policies to prevent malicious websites from accessing sensitive data on other origins. */
+
+// Note --->> middleware check origin you are authorize person or not to access the data
+import cors from "cors"
+import  Express from "express"
+import cookieParser from "cookie-parser";
+
 
 const app = Express();
+/*  */
+/*we also can take empty to cors
+( app.use(cors()); ) if you don't want to configure or setting up */
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,/*'http://example.com', --> we set the URL(domain) jisse ki koi other URL Data access na kr paye */
+    credentials :true
+}))
 
 export {app}
