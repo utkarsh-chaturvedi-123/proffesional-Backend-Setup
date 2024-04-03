1e9cd2ffe938a05e1d45e770c1c421ef467e6c39
utkarsh chaturvedi <utkarshchaturvedi890@gmail.com>
2024-04-03 18:36:37 +0530

    set the middleware

--- src/app.js
+++ src/app.js
@@ -10,10 +10,18 @@ const app = Express();
 /*  */
 /*we also can take empty to cors
 ( app.use(cors()); ) if you don't want to configure or setting up */
+// we set the middleware with --- app.use()
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,/*'http://example.com', --> we set the URL(domain) jisse ki koi other URL Data access na kr paye */
     credentials :true
 }))
+
+
+app.use(Express.json({limit: "16kb"})) /*This middleware used for taking a data from form or json */
+app.use(Express.urlencoded({limit: "16kb"})) /*This middleware used for taking a data from URL and urlencoded -> this will convert the special character of URL  based on ASCII table */
+app.use(Express.static("public")) /*This is used for storing the file in to the public folder */
+
+app.use(cookieParser()) /* Cookies are small piece of information i.e. sent from a website and stored in user's web browser when user browses that website. Every time the user loads that website back, the browser sends that stored data back to website or server, to recognize user. */
 
 export {app}
